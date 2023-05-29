dc29519b7aaac03561c3d184478656b7940af2ca
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-29 14:52:30 +0530

    Update Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,20 +1,24 @@
 import React from 'react'
+import logo from "../media/easy-upload-logo.png";
+import { SiLinkedin } from "react-icons/si";
+import { MdEmail } from "react-icons/md";
+import { BsGithub } from "react-icons/bs";
 
 export const Footer = () => {
   return (
     <>
-        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <footer className="d-flex flex-wrap justify-content-between align-items-center border-top">
     <div className="col-md-4 d-flex align-items-center">
-      <a href="/" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-        <svg className="bi" width="30" height="24"><use href="/bootstrap"></use></svg>
+      <a href="/" className=" me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+        <img  src={logo} style={{ width:"50px"}} alt="" />
       </a>
-      <span className="mb-3 mb-md-0 text-body-secondary">© 2023 Company, Inc</span>
+      <span className="mb-3 mb-md-0 text-body-secondary">© 2023 <span><a href="https://www.linkedin.com/in/sahilfakir/" style={{textDecoration:"none"}} target='blank'>Sahil Fakir</a></span></span>
     </div>
 
     <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-      <li className="ms-3"><a className="text-body-secondary" href="/"><svg className="bi" width="24" height="24"><use href="/twitter"></use></svg></a></li>
-      <li className="ms-3"><a className="text-body-secondary" href="/"><svg className="bi" width="24" height="24"><use href="/instagram"></use></svg></a></li>
-      <li className="ms-3"><a className="text-body-secondary" href="/"><svg className="bi" width="24" height="24"><use href="/facebook"></use></svg></a></li>
+      <li className="ms-3"><a className="text-body-secondary" href="https://www.linkedin.com/in/sahilfakir/" target='blank'> <h3> <SiLinkedin/></h3></a></li>
+      <li className="ms-3"><a className="text-body-secondary" href="mailto:fakirsahil0286@gamil.com"> <h3> <MdEmail/></h3></a></li>
+      <li className="ms-3"><a className="text-body-secondary" href="https://github.com/sahil0286" target='blank'> <h3> <BsGithub/></h3></a></li>
     </ul>
   </footer>
     </>
