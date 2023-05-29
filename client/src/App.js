1fccd4d4b0203012b5440afbdfafaddac276f55d
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-29 10:58:23 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import { useEffect, useRef, useState } from "react";
 import './App.css';
+import { uploadFile } from "./services/api";
 
 function App() {
+  const [file, setFile] = useState("");
+  const [result, setResult] = useState("");
+
+  const fileInputRef= useRef();
+
+
+
+  const onUploadClick=()=>
+  {
+    fileInputRef.current.click();
+  }
+  console.log(file.name)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Simple File Sharing!</h1>
+      <p>Upload and share the download Link</p>
+      <button onClick={()=>onUploadClick()}>Upload</button>
+      <input type="file" ref={fileInputRef} style={{display:"none"}} onChange={(e)=> setFile(e.target.files[0])} />
+      <a href={result}>{result}</a>
+    </>
   );
 }
 
