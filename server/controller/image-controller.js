930aa8de8bd5dfe8e21c14a7935e35751911339c
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-29 10:56:41 +0530

    added deownloadImage

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -16,3 +16,16 @@ export const uploadImage = async (req,res)=>{
     }
 }
 
+export const downloadImage = async (req, res) => {
+    try {
+        const file = await File.findById(req.params.fileId);
+        file.downloadContent++;
+
+        await file.save();
+
+        res.download(file.path,file.name);
+    } catch (error) {
+        console.error(error.message);
+        return res.status(500).json({error: error.message});
+    }
+}
