efea5c63d5a9e3f049b388590a3b1451c4f6c245
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-29 13:05:53 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,14 @@
-import { useEffect, useRef, useState } from "react";
 import './App.css';
-import { uploadFile } from "./services/api";
+import { Footer } from "./components/Footer";
+import Main from "./components/Main";
 
 function App() {
-  const [file, setFile] = useState("");
-  const [result, setResult] = useState("");
 
-  const fileInputRef= useRef();
-
-  useEffect(()=>{
-    const getImage= async ()=>{
-      if(file){
-        const data=new FormData();
-        data.append("name",file.name);
-        data.append("file",file);
-
-      let response = await uploadFile(data);
-      setResult(response.path)
-    }
-  }
-    getImage();
-  },[file])
-
-  const onUploadClick=()=>
-  {
-    fileInputRef.current.click();
-  }
-  console.log(file.name)
   return (
     <>
-      <h1>Simple File Sharing!</h1>
-      <p>Upload and share the download Link</p>
-      <button onClick={()=>onUploadClick()}>Upload</button>
-      <input type="file" ref={fileInputRef} style={{display:"none"}} onChange={(e)=> setFile(e.target.files[0])} />
-      <a href={result}>{result}</a>
+    <div className="container ">
+      <Main/>
+    </div>
     </>
   );
 }
