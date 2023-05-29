a4ece67122fae791b580c9b8710b4840cff22d19
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-29 18:55:21 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const DBconnection = async ()=>{
     // console.log(process.env.PORT);
-    const MONGODB_URI= "mongodb+srv://user:user123@easy-upload.jchdmnj.mongodb.net/?retryWrites=true&w=majority";
+    const MONGODB_URI= "mongodb://user:user123@ac-qljy523-shard-00-00.jchdmnj.mongodb.net:27017,ac-qljy523-shard-00-01.jchdmnj.mongodb.net:27017,ac-qljy523-shard-00-02.jchdmnj.mongodb.net:27017/?ssl=true&replicaSet=atlas-1480sz-shard-0&authSource=admin&retryWrites=true&w=majority";
     try {
         await mongoose.connect(MONGODB_URI,{ useNewUrlParser:true , useUnifiedTopology: true});
         console.log("Database connected Successfully");
