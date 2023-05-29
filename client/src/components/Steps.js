4fe5fcc597789c8c625887cd59e4cacec01ef21c
Sahil Fakir <119412630+sahil0286@users.noreply.github.com>
2023-05-29 14:52:37 +0530

    Update Steps.js

--- client/src/components/Steps.js
+++ client/src/components/Steps.js
@@ -3,9 +3,9 @@ import React from 'react'
 export const Steps = () => {
   return (
   <>
-  <div style={{height:"280px"}}>
+  <div style={{height:"300px"}}>
   </div>
-    <div className="container my-100 px-4 py-5">
+    <div className="container my-100 px-4">
     <h2 className="pb-3 border-bottom text-center ">How to send and share files link with Easy Upload?</h2>
     <div className="row g-4 py-5 row-cols-1 row-cols-lg-3">
       <div className="col d-flex align-items-start">
@@ -37,10 +37,10 @@ export const Steps = () => {
           <svg className="bi" width="1em" height="1em"></svg>
         </div>
         <div>
-          <h4>Step 3: Copy Link for Sharing/Download</h4>
+          <h4>Step 3: Copy Link for Share/Download</h4>
           <ul>
-            <li>Click on the "Copy" button to copy the generated shareable link to the clipboard.</li>
-            <li>You can now paste the link into emails, messages, or any other platform to share or download the uploaded file.</li>
+            <li>Click on the "Copy" button to copy the generated link to the clipboard.</li>
+            <li>You can now paste the link into emails, messages to share or download the uploaded file. 🚀</li>
           </ul>
         </div>
       </div>
